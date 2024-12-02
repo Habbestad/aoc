d8140b5d6c548a70dc4e69675b0137228b67aff0
a504ayu <erik.habbestad@sparebank1.no>
2024-12-02 08:13:33 +0100

    Attempt problem 1 day 2

---
+++ 2/Program.fs
@@ -0,0 +1,21 @@
+﻿
+open System.Text.RegularExpressions
+let parseLine str = 
+    Regex.Split(str, @"\s+") |> Array.map int
+
+let isSafe report = 
+    let pairs = Array.pairwise report
+
+    (pairs |> Array.forall (fun (a, b) -> a < b) 
+    || 
+    pairs |> Array.forall (fun (a, b) -> a > b))
+    &&
+    pairs |> Array.forall (fun (a, b) -> 0 < abs(a-b) && abs(a-b) < 4)
+
+let result1 = "input.txt" 
+              |> System.IO.File.ReadAllLines
+              |> Array.map (parseLine >> isSafe)
+              |> Array.filter id
+              |> Array.length
+
+printfn "Problem 1: %A" result1
