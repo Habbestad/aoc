808e9bc8c899e8481c6eaeb229e0be46fbf0b435
Habbestad <ehabbestad@outlook.com>
2024-12-05 00:00:32 +0100

    Solution day4 part 1

--- 4/Program.fs
+++ 4/Program.fs
@@ -1,29 +1,50 @@
-﻿
-open System.IO
-
-let rows = File.ReadAllLines "input.txt"
-let columnsFromRows (rows : string array) = 
+﻿let transpose (rows : string array) = 
     let n = rows.Length
     [|
         for i in [0..n-1] ->
             [for row in rows -> string row[i]]
             |> String.concat ""
     |]
-    // [|0..n-1|] |> Array.map (fun i -> 
-    //                 rows
-    //                 |> Array.map (fun row -> string row[i] )
-    //                 |> String.concat "" )
-let diagonals (rows : string array) start = 
+
+let upperDiagonals (rows : string array) start = 
     let n = rows.Length
     [|       
         for i in [start..n-1] -> 
-            [| for j in [i..n-i-1] -> string (rows[i][j]) |] 
+            [| for j in [0..n-i-1] -> string (rows[j][i + j]) |] 
             |> String.concat "" 
     |]
 
+let searchAndCount (str : string) = 
+    let check i = 
+        let k = match str[i..(min (i+3) str.Length)] = "XMAS" with
+                | true -> 1
+                | false -> 0
+        let l = match str[(max (i-3) 0) .. i] = "SAMX" with
+                | true -> 1
+                | false -> 0
+        k + l
 
-let firstDiagonals = diagonals rows 0
-let secondDiagonals = diagonals (columnsFromRows rows) 1
-let rowsFlipped = rows |> Array.map (fun row -> string (Array.rev (row.ToCharArray(0,row.Length-1))))
-let firstAntiDiagonals = diagonals rowsFlipped 0
-let secondAntiDiagonals = diagonals (columnsFromRows rowsFlipped) 1
+    [|0 .. str.Length-1|] 
+    |> Array.fold (fun sum i -> 
+            match str[i] with
+            | 'X' -> sum + check i 
+            | _ ->  sum + 0 ) 0
+
+let flip (a : string) = [| for i in a.Length - 1 .. -1 .. 0 -> string a[i]|] 
+                        |> String.concat ""
+
+let rows = System.IO.File.ReadAllLines "input.txt"
+let rowsFlipped = rows |> Array.map flip
+let everything = 
+    [|
+        rows;
+        transpose rows; // columns
+
+        upperDiagonals rows 0; // upper diagonals
+        upperDiagonals (transpose rows) 1; // lower diagonals
+
+        upperDiagonals rowsFlipped 0; // upper antidiagonals
+        upperDiagonals (transpose rowsFlipped) 1; // lower antidiagonals
+    |] |> Array.concat
+
+printfn "Result: %A" (everything |> Array.sumBy searchAndCount)
