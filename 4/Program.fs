b5ae3f4f62976ecc6bec9f3b6409e54212dc49d0
a504ayu <erik.habbestad@sparebank1.no>
2024-12-04 16:12:32 +0100

    fu

--- 4/Program.fs
+++ 4/Program.fs
@@ -2,25 +2,28 @@
 open System.IO
 
 let rows = File.ReadAllLines "input.txt"
-let n = rows.Length
-
-
 let columnsFromRows (rows : string array) = 
-    [|0..n|] |> Array.map (fun i -> 
-                    rows
-                    |> Array.map (fun row -> string row[i] )
-                    |> String.concat "" )
+    let n = rows.Length
+    [|
+        for i in [0..n-1] ->
+            [for row in rows -> string row[i]]
+            |> String.concat ""
+    |]
+    // [|0..n-1|] |> Array.map (fun i -> 
+    //                 rows
+    //                 |> Array.map (fun row -> string row[i] )
+    //                 |> String.concat "" )
 let diagonals (rows : string array) start = 
-    [|start..n-4|] |> Array.map (fun i -> 
-                    rows[i..i+4] 
-                    |> Array.mapi (fun j r -> string r[i+j])
-                    |> String.concat "")
+    let n = rows.Length
+    [|       
+        for i in [start..n-1] -> 
+            [| for j in [i..n-i-1] -> string (rows[i][j]) |] 
+            |> String.concat "" 
+    |]
+
 
 let firstDiagonals = diagonals rows 0
 let secondDiagonals = diagonals (columnsFromRows rows) 1
-let rowsFlipped = rows |> Array.map (fun row -> string (Array.rev (row.ToCharArray(0,n))))
+let rowsFlipped = rows |> Array.map (fun row -> string (Array.rev (row.ToCharArray(0,row.Length-1))))
 let firstAntiDiagonals = diagonals rowsFlipped 0
 let secondAntiDiagonals = diagonals (columnsFromRows rowsFlipped) 1
-
-printfn "%A" secondDiagonals
-
